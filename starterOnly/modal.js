04401853ca79d6c351ef0cb12302e131f9043172
LlevotVincent <vincent.llevot@gmail.com>
2021-05-20 09:22:44 +0200

    ajout des validations formulaire  name et mail

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -12,6 +12,7 @@ const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
 
+
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
 
@@ -27,4 +28,69 @@ const closePopup = document
 
 function closeFormPopup() {
   modalbg.style.display = "none";
+}
+
+
+function checkFirst(text) {
+	if (text.value !== '' && text.value.length >= 2){
+    FirstError.style.display = "none";
+  }else{ 
+    document.getElementById("firstError")
+    FirstError.style.display = "block";
+}}
+
+//code Samy
+document.querySelector(".btn-submit").addEventListener("click", checkForm)
+function checkForm() {
+  console.log("Je vais vérifier le formulaire")
+}
+ 
+function checkContent(type) {
+ 
+  let errorMessages = {
+    first: "Veuillez entrer 2 caractères ou plus pour le champ prénom.",
+    last: "Veuillez entrer 2 caractères ou plus pour le champ nom.",
+    email: "Veuillez entrer une adresse email valide.",
+    date: "Veuillez entrer une date de naissance valide.",
+    quantity: "Le nombre maximum de tournois est 99.",
+    location: "Veuillez choisir une ville.",
+    checkbox: "Veuillez accepter les conditions d'utilisations.",
+  };
+    //first last name validation
+  if (type === "first" || type === "last") {
+    let zoneForm = document.getElementById(type).value;
+    console.log(type)
+    if (zoneForm != null && zoneForm.length >= 1) {
+      console.log("Le ", type, "est bon")
+      document.getElementById(type + "-error").innerText = ""
+    } else {
+      document.getElementById(type + "-error").innerText = errorMessages[type]
+    }
+
+    // email validation
+  } else if (type === "email") {
+    let zoneForm = document.getElementById(type).value;
+    let regMail = /^[a-z\d_\-]+(\.[\a-z\d\-]+)*@[a-z\d\-]+(\.[a-z\d]+)+$/;
+    console.log(type)
+    if (regMail.exec(zoneForm)) {
+      console.log("Le ", type, "est bon")
+      document.getElementById(type + "-error").innerText = ""
+    } else {
+      document.getElementById(type + "-error").innerText = errorMessages[type]
+    }
+
+    // date validation
+  } else if (type === "date") {
+
+    // quantity validation
+  } else if (type === "quantity") {
+    let zoneForm = document.getElementById(type).value;
+    console.log(type)
+    if (zoneForm >= 0 && zoneForm <= 99 ) {
+      console.log("Le ", type, "est bon")
+      document.getElementById(type + "-error").innerText = ""
+    } else {
+      document.getElementById(type + "-error").innerText = errorMessages[type]
+    }
+  }
 }
