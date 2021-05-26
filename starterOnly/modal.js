ea1920e9f0316b3acd3abe710202d0c248d54e1a
LlevotVincent <vincent.llevot@gmail.com>
2021-05-26 15:43:41 +0200

    réalisation de la fonction gloabale + check ville

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -30,135 +30,66 @@ function closeFormPopup() {
   modalbg.style.display = "none";
 }
 
+document.getElementById("Formulaire").addEventListener('submit', checkForm)
+function checkForm () {
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-// check firstname
-document.getElementById("first").addEventListener("change",checkFirstName) 
-
-function checkFirstName() {
+  // Check prénom
   var firstname = document.getElementById("first").value;
-  if (firstname.length == null || firstname.length < 2){
-    console.log('pas assez de caractere');
-    document.getElementById("first-error").innerText = "Veuillez entrer 2 caractères ou plus.";
-    return checkFirstName = false;
-  } else {
-    console.log('prénom bon');
+  if (firstname.length != "" || firstname.length > 2){
     document.getElementById("first-error").innerText = "";
-    return checkFirstName = true;
-  }
-};
-
-// check lastname
-document.getElementById("last").addEventListener("change", checkLastName)
-
-function checkLastName() {
-  var lastname = document.getElementById("last").value;
-  if (lastname.length == null || lastname.length < 2) {
-    console.log('pas assez de caractere');
-    document.getElementById("last-error").innerText = "Veuillez entrer 2 caractères ou plus.";
-    return checkLastName = false;
   } else {
-    console.log('nom bon');
-    document.getElementById("last-error").innerText = "";
-    return checkLastName = true;
+    document.getElementById("first-error").innerText = "Veuillez entrer 2 caractères ou plus.";
+    event.preventDefault();
   }
-};
-
-//check email
-document.getElementById("email").addEventListener("change", checkEmail)
-
-function checkEmail(check) {
-  var mail = check.target.value;
+  // check nom
+  var lastname = document.getElementById("last").value;
+  if (lastname.length != "" || lastname.length > 2) {
+    document.getElementById("last-error").innerText = "";
+  } else {
+    document.getElementById("last-error").innerText = "Veuillez entrer 2 caractères ou plus.";
+    event.preventDefault();
+  }
+  // check email
+  var mail = document.getElementById("email").value;
   var regMail = /^[a-z\d_\-]+(\.[\a-z\d\-]+)*@[a-z\d\-]+(\.[a-z\d]+)+$/;
   if (regMail.exec(mail)) {     //compare les deux données
-    console.log('e-mail bon');
     document.getElementById("email-error").innerText = "";
-    return checkEmail = true;
   } else {
-    console.log('e-mail faux');
     document.getElementById("email-error").innerText = "Veuillez saisir un mail valide";
-    return checkEmail = false;
+    event.preventDefault();
   }
-};
-
-//Check quantité
-document.getElementById("quantity").addEventListener("change", checkQuantity)
-function checkQuantity(check) {
-  var quantityNumber = check.target.value;
+  //Check quantité
+  var quantityNumber = document.getElementById("quantity").value;
   if(quantityNumber > 0 && quantityNumber < 99) {
-    console.log('quantity bonne');
     document.getElementById("quantity-error").innerText = "";
-    return checkQuantity = true;
   } else {
-    console.log('quantity fausse');
     document.getElementById("quantity-error").innerText = "Veuillez rentrer une quantité comprise entre 0 et 99";
-    return checkQuantity = false;
+    event.preventDefault();
   }
-};
-
-//Check Date
-
-document.getElementById("birthdate").addEventListener("change", checkbirthdate)
-function checkbirthdate(check) {
-  var birthdate = check.target.value;
+  // Check Date
+  var birthdate = document.getElementById("birthdate").value;
   if(birthdate !=="") {
-    console.log('birthdate bonne');
     document.getElementById("birthdate-error").innerText = "";
-    return checkbirthdate = true;
   } else {
-    console.log('birthdate fausse');
     document.getElementById("birthdate-error").innerText = "Veuillez renseigner une date";
-    return checkbirthdate = false;
+    event.preventDefault();
   }
-};
-
-//Check City
-
-const location1 = document.getElementById("location1");
-const location2 = document.getElementById("location2");
-const location3 = document.getElementById("location3");
-const location4 = document.getElementById("location4");
-const location5 = document.getElementById("location5");
-const location6 = document.getElementById("location6");
-
-function checklocation (){
-
-
-  if (location1.checked === true) {
-    console.log('c est coché');
-    return checklocation = true;
+  // Check ville
+  var location1 = document.getElementById("location1");
+  var location2 = document.getElementById("location2");
+  var location3 = document.getElementById("location3");
+  var location4 = document.getElementById("location4");
+  var location5 = document.getElementById("location5");
+  var location6 = document.getElementById("location6");
+  if(location1.checked||location2.checked||location3.checked||location4.checked||location5.checked||location6.checked) {
+    document.getElementById("location-error").innerText = "";
   } else {
-    return checklocation = false
+    document.getElementById("location-error").innerText = "Veuillez cocher une ville";
+    event.preventDefault();
   }
-}
 
 
-//validation Formulaire
 
-document.getElementById("Formulaire").addEventListener("submit", validation)
-function validation(e) {
-  if (checkFirstName === true 
-    && checkLastName === true 
-    && checkEmail === true
-    && checkQuantity === true
-    && checkbirthdate === true
-    && checklocation === true) {
-    console.log('le formulaire est valide');
-    }
-  else {
-    console.log('le formulaire est non valide');
-    e.preventDefault();
-  }
-}
+
+
+};
